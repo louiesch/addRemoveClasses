b3848b0d0dbb0050ab216184163eefa10ede2799
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-12 13:52:31 -0800

    add jQuery removes and adds classes and changes the color
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
   $("button#green").click(function() {
+    $("body").removeClass();
+    $("#red-box").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
+    $("#red-box").addClass("red-background");
   });
 
   $("button#red").click(function() {
+    $("body").removeClass();
+    $("#red-box").removeClass();
     $("body").addClass("red-background");
   });
+
+ 
 });
